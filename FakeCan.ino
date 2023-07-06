5ac9cd9822b3a4b243ed0512f8c102354426f90a
Tomasz Dobrowolski <chrumck@gmail.com>
2023-07-06 11:29:35 +0200

    More work on ND frames, disable send if serial not connected

--- FakeCan.ino
+++ FakeCan.ino
@@ -9,7 +9,7 @@
 #define IS_DEBUG
 
 #ifdef IS_DEBUG
-#define CAN_SEND_INTERVAL 1000
+#define CAN_SEND_INTERVAL 100
 #else
 #define CAN_SEND_INTERVAL 10
 #endif
@@ -34,12 +34,9 @@ void setup() {
 }
 
 const u16 frameIdsToSend[] = {
-    0x202, 0x41,
-    0x118, 0x138, 0x139, 0x13B, 0x13C,
-    0x202, 0x41,
-    0x143, 0x146,
-    0x241,
-    0x345,
+    0x202, 0x215, 0x165, 0x240, 0x78, 0x86,
+    0x415, 0x420,
+    0x202, 0x215, 0x165, 0x240, 0x78, 0x86
 };
 
 const u16 framesCount = sizeof(frameIdsToSend) / sizeof(frameIdsToSend[0]);
@@ -47,6 +44,8 @@ const u16 framesCount = sizeof(frameIdsToSend) / sizeof(frameIdsToSend[0]);
 void loop() {
     static u16 frameToSendIndex = 0;
     static u32 lastFrameSentTime = millis();
+
+    if (!Serial) return;
 
     u32 currentTime = millis();
     if (currentTime - lastFrameSentTime < CAN_SEND_INTERVAL) return;
@@ -94,54 +93,11 @@ boolean sendFrame(u16 id, u8* payload, u8 length) {
 void generateFramePayload(u16 pid, u8* payload) {
     switch (pid) {
     case 0x202: {
-        u16 rpm = 3456;
-        payload[1] = rpm & 0xFF;
-        payload[0] = (rpm >> 8) & 0xFF;
+        u16 rpm = (880 + random(0, 100)) + ((millis() % 6000) < 3000 ? 2200 : 0);
+        u16 raw = rpm * 4;
+        payload[1] = raw & 0xFF;
+        payload[0] = (raw >> 8) & 0xFF;
 
-        break;
-    }
-
-    case 0x138: {
-        // Pretend that the steering wheel is turned by 123 degrees to the left
-        int16_t steeringAngleDegrees = -123;
-        int16_t steeringValue = steeringAngleDegrees * 10;
-        payload[2] = steeringValue & 0xFF;
-        payload[3] = (steeringValue >> 8) & 0xFF;
-
-        // TODO: Verify the scale for this value. The current scale is suspicious,
-        // but matches real-world testing so far. Need to go to a skid pad to
-        // verify for sure.
-        float yawRateDegreesPerSecond = -12.3;
-        int16_t yawRateValue = (int16_t)(-yawRateDegreesPerSecond / 0.2725);
-        payload[4] = yawRateValue & 0xFF;
-        payload[5] = (yawRateValue >> 8) & 0xFF;
-        break;
-    }
-
-    case 0x139: {
-        u16 speedMetersPerSecond = 10;  // 36 km/h, ~22.4 mph.
-        // The encoding seems to be roughly radians per second x100.
-        // The coefficient was tuned by comparing the values against an external
-        // GPS from a session where I drove in a straight line on a highway at
-        // constant speed on cruise control.
-        u16 speedValue = (u16)(speedMetersPerSecond * 63.72);
-        payload[2] = speedValue & 0xFF;
-        payload[3] = (speedValue >> 8) & 0xFF;
-
-        // The units used for the master brake cylinder pressure are believed to
-        // be 1/128 kPa.
-        float brakePressureKpa = 1024;
-        payload[4] = 0x0C;
-        payload[5] = (u8)(brakePressureKpa / 128);
-        break;
-    }
-
-    case 0x345: {
-        u8 oilTempC = 100;
-        payload[3] = oilTempC + 40;
-
-        u8 coolantTempC = 90;
-        payload[4] = coolantTempC + 40;
         break;
     }
 
